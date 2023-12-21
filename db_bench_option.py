148dddafd48c9af05a5c26e1320e59cc226e425b
Rhys <811437508@qq.com>
2023-12-22 00:19:32 +0800

    add db bench path

--- db_bench_option.py
+++ db_bench_option.py
@@ -7,7 +7,7 @@ default_cfg = ConfigParser()
 default_cfg.read("default.ini")
 
 SUDO_PASSWD = ""  #default_cfg.get("Permission", "passwd")
-DEFAULT_DB_BENCH = "/home/hybridrocks/db_bench" #default_cfg.get("Paths", "db_bench_path")
+DEFAULT_DB_BENCH = "/users/Ruisong/hybridrocks/db_bench" #default_cfg.get("Paths", "db_bench_path")
 
 CPU_RESTRICTING_TYPE = 0 #int(default_cfg.get("CPU", "limit_type"))
 
