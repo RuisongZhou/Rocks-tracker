cd013cf649b8e62ef333770d26700f023ae71ce9
Rhys <811437508@qq.com>
2024-03-27 00:10:20 +0800

    add exp

--- hybird_influence/hybrid_nvme_ssd.py
+++ hybird_influence/hybrid_nvme_ssd.py
@@ -6,7 +6,7 @@ os.chdir("../")
 print(os.getcwd())
 sys.path.insert(0, '.')
 
-from db_bench_option import DEFAULT_DB_BENCH, DEFAULT_L1_SIZE, DEFAULT_COMPACTION_TRIGGER
+from db_bench_option import DEFAULT_DB_BENCH, DEFAULT_L1_SIZE, DEFAULT_COMPACTION_TRIGGER, DEFAULT_ENTRY_COUNT
 from db_bench_option import load_config_file
 from db_bench_option import set_parameters_to_env
 from db_bench_runner import DB_launcher
@@ -21,25 +21,42 @@ if __name__ == '__main__':
     parameter_dict = load_config_file('config.json')
     set_parameters_to_env(parameter_dict, env)
 
-    result_dir = "results_ssd_hdd_hybrid/"
+    result_dir = "results_nvme_ssd_hybrid/"
     fast_device_size_base_list = parameter_dict["fast_device_size"] 
     for fast_device_size_base in fast_device_size_base_list:    
-        target_result_dir = result_dir+str(fast_device_size_base)
+        target_result_dir = result_dir+ "f" + str(fast_device_size_base)  + "_v128"
         # L0 256M L1 2560M L2 25.6G L3 256000M
-        fast_size = fast_device_size_base * 1024 * 1024 * 1024
-        slow_size = 200 * 1024 * 1024 * 1024
+        slow_size = DEFAULT_ENTRY_COUNT * (128 + 8)
+        fast_size = int(slow_size * fast_device_size_base_list / 100)
         ssd_path = parameter_dict["hybrid_storage_paths"]["SATASSD"]
-        hdd_path = parameter_dict["hybrid_storage_paths"]["SATAHDD"]
         nvme_path = parameter_dict["hybrid_storage_paths"]["NVMESSD"]
         print(target_result_dir)
         runner = DB_launcher(
             env, target_result_dir, db_bench=DEFAULT_DB_BENCH, extend_options={
                 "db_path": nvme_path+":"+str(fast_size)+","+ssd_path+":"+str(slow_size),
-                "value_size":256,
-                "key_size":16,
+                "value_size":128,
+                "key_size":8,
                 "report_interval_seconds": 1,
                 "duration": 7200,       # 120min
-                "benchmarks":"fillbatch,stats,resetstats,fillrandom,stats",
+                "benchmarks":"fillseq,stats,resetstats,fillrandom,stats",
+                "statistics":"true",
+            })
+        runner.run()
+
+ 
+    for value_size in [16, 32, 64, 256, 512, 1024]:
+
+        target_result_dir = result_dir + "f1_v" + str(value_size)
+        slow_size = DEFAULT_ENTRY_COUNT * (128 + 8)
+        fast_size = int(slow_size * 0.1)
+        runner = DB_launcher(
+            env, target_result_dir, db_bench=DEFAULT_DB_BENCH, extend_options={
+                "db_path": nvme_path+":"+str(fast_size)+","+ssd_path+":"+str(slow_size),
+                "value_size":value_size,
+                "key_size":8,
+                "report_interval_seconds": 1,
+                "duration": 7200,       # 120min
+                "benchmarks":"fillseq,stats,resetstats,fillrandom,stats",
                 "statistics":"true",
             })
         runner.run()
