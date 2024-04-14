954cd5179406cf74d8e3ee232ff9afbfa929124f
Rhys <811437508@qq.com>
2024-04-14 22:14:22 +0800

    fxi bugs

--- hybird_influence/hybird_size.py
+++ hybird_influence/hybird_size.py
@@ -29,22 +29,6 @@ if __name__ == '__main__':
     target_result_dir = result_dir + "exp5_ratio_0.1_fill"
     slow_size = DEFAULT_DB_SIZE
     fast_size = int(slow_size * 0.1)
-    runner = DB_launcher(
-            env, target_result_dir, db_bench=DEFAULT_DB_BENCH, extend_options={
-                "db_path": nvme_path+":"+str(fast_size)+","+ssd_path+":"+str(slow_size),
-                "value_size": 128,
-                "num": int(DEFAULT_DB_SIZE / 128),
-                "key_size":8,
-                "report_interval_seconds": 1,
-                "ycsb_readwritepercent":0,
-                "max_background_compactions": 16,  #额外一个flush线程
-                "zipf_const": 0,
-                "benchmarks":"ycsbfilldb,stats",
-                "statistics":"true",
-                "wal_bytes_per_sync" : 0,
-                "max_background_flushes" : 2
-            })
-    runner.run()
 
     for thread_num in [16, 8, 4, 2, 1]:
         target_result_dir = result_dir + "exp5_ratio0.1" + "thread_" + str(thread_num) 
@@ -60,7 +44,7 @@ if __name__ == '__main__':
                 "max_background_compactions": thread_num + 1,  #额外一个flush线程
                 "use_direct_io_for_flush_and_compaction": True,
                 "zipf_const": 0,
-                "benchmarks":"ycsbwklda,stats",
+                "benchmarks":"ycsbfilldb,stats,resetstats,ycsbwklda,stats",
                 "statistics":"true",
                 "use_existing_db" : True,
                 "duration" : 1800,
